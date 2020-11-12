8247f305b3680aac05674c06a78671c8081d4e4b
zhangshili <zhangshili@didichuxing.com>
2020-11-12 15:24:09 +0800

    添加listView支持

--- app/src/main/java/sample/kingja/loadsir/rtl/RtlMainActivity.java
+++ app/src/main/java/sample/kingja/loadsir/rtl/RtlMainActivity.java
@@ -5,6 +5,11 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.Nullable;
 import sample.kingja.loadsir.R;
@@ -16,13 +21,32 @@ import sample.kingja.loadsir.rtl.utils.RtlUtils;
 
 public class RtlMainActivity extends Activity {
 
+    private ListView listView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rtl_main);
+        initView();
+        bindData();
         Log.e("stormzsl", "shouldUseLayoutRtl:"+RtlUtils.getInstance().shouldUseLayoutRtl(this));
     }
 
+
+
+    private void initView(){
+        listView=findViewById(R.id.listview);
+
+    }
+
+    private void bindData() {
+        List<String> List = new ArrayList<String>();//声明集合
+        List.add("列表1");
+        List.add("列表2");
+        List.add("列表3");
+
+       ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, List);
+        listView.setAdapter(adapter);
+    }
 
 
 }
