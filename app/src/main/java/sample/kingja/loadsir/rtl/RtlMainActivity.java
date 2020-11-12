1b6cd26108a0c49befe220c463c32a166e79c7bb
zhangshili <zhangshili@didichuxing.com>
2020-11-12 14:55:45 +0800

    添加RtlUtils

--- app/src/main/java/sample/kingja/loadsir/rtl/RtlMainActivity.java
+++ app/src/main/java/sample/kingja/loadsir/rtl/RtlMainActivity.java
@@ -1,10 +1,14 @@
 package sample.kingja.loadsir.rtl;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import sample.kingja.loadsir.R;
+import sample.kingja.loadsir.rtl.utils.RtlUtils;
 
 /**
  * 作者:created by storm
@@ -16,5 +20,9 @@ public class RtlMainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rtl_main);
+        Log.e("stormzsl", "shouldUseLayoutRtl:"+RtlUtils.getInstance().shouldUseLayoutRtl(this));
     }
+
+
+
 }
--- app/src/main/java/sample/kingja/loadsir/rtl/utils/RtlUtils.java
+++ app/src/main/java/sample/kingja/loadsir/rtl/utils/RtlUtils.java
@@ -1,8 +1,30 @@
 package sample.kingja.loadsir.rtl.utils;
 
+import android.content.Context;
+import android.content.res.Configuration;
+import android.view.View;
+
 /**
  * 作者:created by storm
  */
 
 public class RtlUtils {
+    private static final RtlUtils INSTANCE = new RtlUtils();
+
+    public static RtlUtils getInstance() {
+        return INSTANCE;
+    }
+
+    private RtlUtils() {
+    }
+
+    public boolean shouldUseLayoutRtl(Context context) {
+        final Configuration config = context.getApplicationContext().getResources().getConfiguration();
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            return View.LAYOUT_DIRECTION_RTL == config.getLayoutDirection();
+        } else {
+            return false;
+        }
+    }
 }
