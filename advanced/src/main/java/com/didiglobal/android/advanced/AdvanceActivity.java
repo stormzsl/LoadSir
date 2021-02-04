a1d4040923d9063d7531a04f0f32f1f5b3b2a641
zhangshili <zhangshili@didichuxing.com>
2021-02-04 20:51:00 +0800

    计算View的总数

--- advanced/src/main/java/com/didiglobal/android/advanced/AdvanceActivity.java
+++ advanced/src/main/java/com/didiglobal/android/advanced/AdvanceActivity.java
@@ -2,7 +2,9 @@ package com.didiglobal.android.advanced;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.didiglobal.android.advanced.hook.HookViewClickActivity;
 import com.didiglobal.android.advanced.monitor.FrameMonitor;
@@ -16,12 +18,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class AdvanceActivity extends AppCompatActivity {
 
+    private View rootView;
+
+    private static final String TAG=AdvanceActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advance);
-        FrameMonitor monitor=new FrameMonitor();
+        FrameMonitor monitor = new FrameMonitor();
         monitor.caculateFrame();
+        rootView=findViewById(R.id.viewroot);
+        int maxLevel=calculateViewTreeTotalLevel(rootView);
+        Log.e(TAG,"rootview maxLevel is="+maxLevel);
     }
 
     public void hookViewClick(View view) {
@@ -29,9 +38,32 @@ public class AdvanceActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void collectMethodStackClick(View view){
+    public void collectMethodStackClick(View view) {
         Intent intent = new Intent(this, CollectMethodStackActivity.class);
         startActivity(intent);
+    }
+
+    //计算View树层级总数
+    private int calculateViewTreeTotalLevel(View view) {
+        if (!(view instanceof ViewGroup)) {
+            return 1;
+        }
+
+        ViewGroup viewGroup = (ViewGroup) view;
+        if (viewGroup.getChildCount() == 0) {
+            return 1;
+        }
+
+        int maxLevel = 0;
+        for (int i = 0; i < viewGroup.getChildCount(); i++) {
+            View childView = viewGroup.getChildAt(i);
+            int childViewLevel = calculateViewTreeTotalLevel(childView)+1;
+            if (childViewLevel > maxLevel) {
+                maxLevel = childViewLevel;
+            }
+        }
+
+        return maxLevel;
     }
 
 }
